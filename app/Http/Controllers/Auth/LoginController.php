eba5bf0ae17417d9afaccf113c7ed8cd7a0c43df
urospolovina <uros.polovina@gmail.com>
2022-11-06 10:33:21 +0100

    login controller

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -8,10 +8,10 @@ use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    public function __invoke(Request $request)
-    {
-        if (!auth()->attempt($request->only('email', 'password'))){
-            throw new AuthenticationException();
-        }
-    }
+    // public function __invoke(Request $request)
+    // {
+    //     if (!auth()->attempt($request->only('email', 'password'))){
+    //         throw new AuthenticationException();
+    //     }
+    // }
 }
