8db9a72cb78b3f236dcfd6f6326ea6850461a05b
urospolovina <uros.polovina@gmail.com>
2022-11-06 11:30:18 +0100

    selja

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -8,7 +8,9 @@ use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    public function __invoke(Request $request)
+
+
+    public function selja(Request $request)
     {
         if (!auth()->attempt($request->only('email', 'password'))){
             throw new AuthenticationException();
