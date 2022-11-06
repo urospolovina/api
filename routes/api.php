ccec34981e152625a5e43fd18bed8f76e1ff1b70
urospolovina <uros.polovina@gmail.com>
2022-11-06 12:23:32 +0100

    nesto

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/login', [LoginController::class, 'selja']);
+Route::post('/login', [LoginController::class, 'selja'] );
